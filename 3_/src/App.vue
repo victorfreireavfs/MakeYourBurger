ff0d02879ff70d103f87c8ae1cf7af4711c1d1df
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-08 21:28:15 -0300

    Estilizando o navbar

--- 3_/src/App.vue
+++ 3_/src/App.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
-      <Navbar/>
+      <Navbar :logo="logo_src" :alt="app_name" id="logo_url"/>
       <router-view />
       <Footer />
     </div>
-  
-</template>
+  </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
@@ -16,6 +15,12 @@ export default {
   components:{
     Navbar,
     Footer
+  },
+  data(){
+    return{
+      logo_src:"/img/logo.png",
+      app_name: "Make Your Burger"
+    }
   }
 }
 </script>
--- 3_/src/components/Navbar.vue
+++ 3_/src/components/Navbar.vue
@@ -1,19 +1,34 @@
 <template>
-    <div id="nav">
-        <router-link to="/">
-            <img src=""  id="logo">
+    <div id="nav" class="nav">
+        <router-link to="/" id="logo_url">
+            <img :src="logo" :alt="alt" id="logo" >
         </router-link>
         <router-link to="/">Home</router-link> |
         <router-link to="/pedidos">Pedidos</router-link>
-    
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    props: ["logo", "alt"]
 }
 </script>
 
-<style>
+<style scoped>
+
+.nav{
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 15px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+
+#logo_url{
+    margin: auto;
+    margin-left: 0;
+}
 </style>
